6bf2919d0118655361eba35bbad95e72b87d981f
Sandeep kumar Sharma <57533185+Sandeep9199@users.noreply.github.com>
2024-08-08 08:51:03 +0530

    Inheritance

--- 10-TS-Inheritance/app.ts
+++ 10-TS-Inheritance/app.ts
@@ -1,50 +1,48 @@
 class BasicCalc {
-    public result : number;
+  public result: number;
 
-    constructor() {
-        this.result = 0;
-    }
+  constructor() {
+    this.result = 0;
+  }
 
-    // sum
-    public sum(a:number , b:number):void{
-        this.result = a + b;
-        console.log(this.result);
-    }
+  // sum
+  public sum(a: number, b: number): void {
+    this.result = a + b;
+    console.log(this.result);
+  }
 
-    // sub
-    public sub(a:number , b:number):void{
-        this.result = a - b;
-        console.log(this.result);
-    }
+  // sub
+  public sub(a: number, b: number): void {
+    this.result = a - b;
+    console.log(this.result);
+  }
 }
 /*let basicCalc = new BasicCalc();
 basicCalc.sum(10,20);
 basicCalc.sub(10,20);*/
+
 // super - sub , parent - child , base - derived
-class AdvCalc extends BasicCalc{
+class AdvCalc extends BasicCalc {
+  constructor() {
+    super(); // calling parent class constructor
+  }
 
-    constructor() {
-        super(); // calling parent class constructor
-    }
+  // sum (over ridding - extend)
+  public sum(a: number, b: number): void {
+    this.result = a + a + b + b;
+    console.log(this.result);
+  }
 
-    // sum (over ridding - extend)
-    public sum(a:number , b:number):void{
-        this.result = a + a + b + b;
-        console.log(this.result);
-    }
-
-    // mul
-    public mul(a:number , b:number):void{
-        this.result = a * b;
-        console.log(this.result);
-    }
-
+  // mul
+  public mul(a: number, b: number): void {
+    this.result = a * b;
+    console.log(this.result);
+  }
 }
 // child
 let advCalc = new AdvCalc();
-advCalc.sum(10,20); // 60
-advCalc.mul(10,20);
+advCalc.sum(10, 20); // 60
+advCalc.mul(10, 20);
 
 // parent
 let basicCalc = new BasicCalc();
-
